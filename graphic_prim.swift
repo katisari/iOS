a3699836464e49ffbd0e78a363d8bda6599f1539
katisari <39178730+katisari@users.noreply.github.com>
2018-07-03 15:56:54 -0700

    Update graphic_prim.swift

--- graphic_prim.swift
+++ graphic_prim.swift
@@ -13,15 +13,16 @@ struct Line{
     var Start: Point
     var End: Point
     var length: Double
-    init(FirstPoint: Point, SecondPoint: Point, length: Double) {
-        self.length=length
-        Start = FirstPoint
-        End = SecondPoint
+    init(FirstPoint: Point, SecondPoint: Point) {
+        self.length = (pow((SecondPoint.X-FirstPoint.X), 2) + pow((SecondPoint.Y-FirstPoint.Y), 2)).squareRoot()
+        self.Start = FirstPoint
+        self.End = SecondPoint
     }
     func lengthofline() -> Double{
         return length
     }
 }
+
 
 struct Triangle{
     var Points: [Point] = [Point]()
